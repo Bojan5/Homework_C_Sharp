7fa71b12696f5244fad889235b81ae1cab849569
Bojan5 <60651119+Bojan5@users.noreply.github.com>
2020-03-12 19:29:19 +0100

    Domasno

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HOMEWORK 01
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+            Console.ReadLine();
+
+
+        }
+    }
+}
